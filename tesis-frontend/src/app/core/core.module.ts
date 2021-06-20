0a75c5d2f1beb7c00e4eebf14b7acee1ca695b9d
AgusGelos <juanagustingelos1@gmail.com>
2021-06-20 17:39:20 -0300

    Registrar/modificar datos completado

--- tesis-frontend/src/app/core/core.module.ts
+++ tesis-frontend/src/app/core/core.module.ts
@@ -1,21 +1,25 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { UserRegisterComponent } from './components/user-register/user-register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { UserCompletedDataComponent } from './components/user-completed-data/user-completed-data.component';
 
 
 @NgModule({
   declarations: [
-    UserRegisterComponent
+    UserRegisterComponent,
+    UserCompletedDataComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+
 
   ],
-  exports:  [ UserRegisterComponent ],
+  exports:  [ UserRegisterComponent, UserCompletedDataComponent ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [DatePipe]
 })
 export class CoreModule { }
--- tesis-frontend/src/app/core/services/user.service.ts
+++ tesis-frontend/src/app/core/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { IUser } from '../models/IUser';
+import { IUserComplete } from '../models/IUserComplete';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,10 @@ export class UserService {
 
   postUser(user: IUser): any {
     return this.httpClient.post(this.apiURL, user);
+  }
+
+  postUserComplete(user: IUserComplete): any {
+    return this.httpClient.put(this.apiURL, user);
   }
 
 }
