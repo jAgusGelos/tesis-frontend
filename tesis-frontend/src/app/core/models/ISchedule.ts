2322d72c4e9eef5f9dc0c73d3326a5cc06cf07d6
AgusGelos <juanagustingelos1@gmail.com>
2021-07-24 09:28:14 -0300

    Schedule instead of agenda

--- tesis-frontend/src/app/core/models/ISchedule.ts
+++ tesis-frontend/src/app/core/models/ISchedule.ts
@@ -1,11 +1,15 @@
-export interface IAgenda {
-  simposios: number[];
+export interface ISchedule {
+  Id: number;
   fechaInCongreso: Date;
   fechaFinCongreso: Date;
+  fechaInInscrip: Date;
+  fechaFinInscrip: Date;
+  fechaFinInscripTardia: Date;
   fechaLimPapers: Date;
   fechaProrrogaPapers: Date;
   fechaFinEvaluacion: Date;
   fechaFinReEv: Date;
+  simposios: number[];
   aulas: number[];
   modalidad: string;
 }
