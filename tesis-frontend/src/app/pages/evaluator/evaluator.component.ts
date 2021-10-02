5c69ef29933f089570b1032ec8e0679dbb9f0794
AgusGelos <juanagustingelos1@gmail.com>
2021-10-02 16:59:35 -0300

    evaluation evaluator + search

--- tesis-frontend/src/app/pages/evaluator/evaluator.component.ts
+++ tesis-frontend/src/app/pages/evaluator/evaluator.component.ts
@@ -13,6 +13,9 @@ export class EvaluatorComponent implements OnInit {
   adding = false;
   formEvaluator: FormGroup;
   evaluatorSelectedId = [];
+  notLoaded = true;
+  showListSelected = [];
+  showListNotSelected = [];
 
   constructor(
     private evaluatorService: EvaluatorService
@@ -24,11 +27,13 @@ export class EvaluatorComponent implements OnInit {
     this.getEvaluators();
   }
 
+  /**
+   * Carga los evaluadores no seleccionados
+   */
   getEvaluators(): void {
-
     this.evaluatorService.getEvaluatorsSimposio().subscribe((res: any) => {
       if (res.data.length === 0) {
-        alert('Lo sentimos, actualmente no disponemos de más evaluadores.');
+        alert('Lo sentimos, actualmente no disponemos de evaluadores.');
         return;
       }
       this.evaluatorList = res.data.filter((x: any) => {
@@ -36,17 +41,27 @@ export class EvaluatorComponent implements OnInit {
           return x;
         }
       });
+      this.showListNotSelected = this.evaluatorList.slice();
     });
   }
 
+  /**
+   * Carga los evaluadores ya seleccionados
+   */
   getEvaluatorSelected(): void {
     this.evaluatorService.getEvaluatorsGroup().subscribe((res: any) => {
       this.evaluatorSelectedList = res.data;
       this.evaluatorSelectedId = res.data.map((x: any) => x.idEvaluador);
+      this.showListSelected = res.data;
     });
 
   }
 
+  /**
+   *
+   * @param item Evaluador
+   * Selecciona un nuevo evaluador
+   */
   addEvaluator(item: any): void {
     this.evaluatorService.postEvaluatorGroup(item.idEvaluador).subscribe((res: any) => {
       this.evaluatorList = this.evaluatorList.filter((x: any) => {
@@ -54,18 +69,25 @@ export class EvaluatorComponent implements OnInit {
           return x;
         }
       });
+      this.showListNotSelected = this.evaluatorList.slice();
       this.evaluatorSelectedList.push(item);
+      this.showListSelected.push(item);
     });
   }
 
+  /**
+   * Carga todos los evaluadores
+   */
   loadAll(): void {
     if (confirm('Atención, los evaluadores que está por cagar no pertenecen a su simposio. ¿Desea hacerlo de todas formas?')) {
       this.evaluatorService.getEvaluatorsAll().subscribe((res: any) => {
+        this.notLoaded = false;
         this.evaluatorList = res.data.filter((x: any) => {
           if (!this.evaluatorSelectedId.includes(x.idEvaluador)) {
             return x;
           }
         });
+        this.showListNotSelected = this.evaluatorList.slice();
       });
     }
 
@@ -81,13 +103,25 @@ export class EvaluatorComponent implements OnInit {
     ) {
       this.evaluatorService.deleteEvaluatorGroup(item.idEvaluador).subscribe((res: any) => {
         this.evaluatorList.push(item);
+
         this.evaluatorSelectedList = this.evaluatorSelectedList.filter((x: any) => {
           if (x.idEvaluador !== item.idEvaluador) {
             return item;
           }
         });
+        this.showListNotSelected.push(item);
+        this.showListSelected = this.evaluatorSelectedList.slice();
     });
 
     }
   }
+
+  searchSelected(filterList): void {
+    this.showListSelected = filterList;
+  }
+
+  searchNotSelected(filterList): void {
+    this.showListNotSelected = filterList;
+  }
+
 }
