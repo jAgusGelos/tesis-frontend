550a3a285e379b6ab25f92af5fbcdd747fa33839
AgusGelos <juanagustingelos1@gmail.com>
2021-10-09 11:01:54 -0300

    i traifa solved

--- src/app/pages/tarifas/tarifas.component.ts
+++ src/app/pages/tarifas/tarifas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ITarifa } from 'src/app/core/models/itarifa';
+import { ITarifa } from 'src/app/core/models/ITarifa';
 import { TarifasService } from 'src/app/core/services/tarifas.service';
 
 
