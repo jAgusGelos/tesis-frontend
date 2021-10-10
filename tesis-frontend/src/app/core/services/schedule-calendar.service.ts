ba4b269166190717aa7cb1d3ec0d46f6493e0c1a
AgusGelos <juanagustingelos1@gmail.com>
2021-10-10 19:20:55 -0300

    apiULr

--- tesis-frontend/src/app/core/services/schedule-calendar.service.ts
+++ tesis-frontend/src/app/core/services/schedule-calendar.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CalendarSchedulerEvent, CalendarSchedulerEventAction, CalendarSchedulerEventStatus } from 'angular-calendar-scheduler';
 import { addDays, addHours, addMinutes, setHours, startOfHour, subHours, subMinutes } from 'date-fns';
+import { environment } from 'src/environments/environment';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { AuthService } from './auth.service';
 })
 export class ScheduleCalendarService {
 
-  apiUrl = '';
+  apiUrl = environment.apiURL;
   idCongreso = null;
   constructor(private httpClient: HttpClient,
               private auth: AuthService) {
