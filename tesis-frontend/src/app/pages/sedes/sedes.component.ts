bc30b265e7a54bf3819790a72db5b42b8ddf240f
AgusGelos <juanagustingelos1@gmail.com>
2021-12-07 18:47:36 -0300

    room fixed

--- tesis-frontend/src/app/pages/sedes/sedes.component.ts
+++ tesis-frontend/src/app/pages/sedes/sedes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ISede } from 'src/app/core/models/ISede';
 import { SedesService } from 'src/app/core/services/sedes.service';
 import { UserService } from 'src/app/core/services/user.service';
@@ -12,13 +13,19 @@ export class SedesComponent implements OnInit {
 
   new = false;
   edit = false;
+  loaded = false
   provincias = [];
   localidades = [];
   sedeList: ISede[] = [];
   sede: ISede;
 
   constructor(private sedeService: SedesService,
-              private userService: UserService) { }
+              private userService: UserService,
+              private router: Router) {
+                this.router.routeReuseStrategy.shouldReuseRoute = () => {
+                  return false;
+                };
+              }
 
   ngOnInit(): void {
     this.getSedes();
@@ -29,27 +36,30 @@ export class SedesComponent implements OnInit {
     this.sedeList = [];
     this.sedeService.getSedes().subscribe((res: any) => {
       this.sedeList = res.data;
+      this.loaded = true;
     });
   }
 
   postSede(item): void {
     this.sedeService.postSede(item).subscribe((res: any) => {
       this.toggleEdit();
-      this.getSedes();
+      this.router.navigateByUrl('/sedes');
     },
     (err: any) => console.log(err));
   }
 
   putSede(item): void {
     this.sedeService.putSede(item).subscribe((res: any) => {
-      this.getSedes();
+
       this.toggleEdit();
+      this.router.navigateByUrl('/sedes');
     });
   }
 
   deleteSede(id): void {
     this.sedeService.deleteSede(id).subscribe((res: any) => {
-      this.getSedes();
+
+      this.router.navigateByUrl('/sedes');
     });
   }
 
